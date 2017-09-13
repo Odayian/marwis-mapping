4b2f74f04c7c1bf866fc12c8e91a7075f0487b06
Benjamin Durbin <bwdurbin@gmail.com>
2017-09-13 14:51:17 +0000

    added script sources

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,24 @@
 /*global angular*/
-angular.module('marwisMapping', []);
+(function() {
+    function config($locationProvider, $stateProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+             });
+ 
+        $stateProvider
+            .state('mapping', {
+                url: '/',
+                controller: 'MapCtrl as map',
+                templateUrl: '/templates/lufft.html'
+            });
+
+
+    }
+    
+   
+     angular
+         .module('marwisMapping', ['ui.router','ui.bootstrap', 'firebase'])
+         .config(config);
+ })();
